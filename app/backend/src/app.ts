0d28aec7633e8f9796085731ec85fa0d8fb7e263
Gabriel Silvestre <gabriel.h.silvestre11@gmail.com>
2022-05-30 17:42:16 -0300

    feat(src/app): start app and create test route

--- app/backend/src/app.ts
+++ app/backend/src/app.ts
@@ -5,15 +5,19 @@ class App {
   // ...
 
   constructor() {
-    // ...
+    this.app = express();
     this.config();
+    this.routes();
     // ...
   }
 
-  private config():void {
+  private config(): void {
     const accessControl: express.RequestHandler = (_req, res, next) => {
       res.header('Access-Control-Allow-Origin', '*');
-      res.header('Access-Control-Allow-Methods', 'GET,POST,DELETE,OPTIONS,PUT,PATCH');
+      res.header(
+        'Access-Control-Allow-Methods',
+        'GET,POST,DELETE,OPTIONS,PUT,PATCH',
+      );
       res.header('Access-Control-Allow-Headers', '*');
       next();
     };
@@ -23,8 +27,12 @@ class App {
   }
 
   // ...
-  public start(PORT: string | number):void {
-    // ...
+  public start(PORT: string | number): void {
+    this.app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
+  }
+
+  private routes(): void {
+    this.app.get('/', (_req, res) => res.json({ message: 'Hello World!' }));
   }
 }
 
