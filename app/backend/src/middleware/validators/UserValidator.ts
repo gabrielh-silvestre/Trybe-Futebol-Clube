e17b635a9425cfec028a6f22b6d4b676c09abc07
Gabriel Silvestre <gabriel.h.silvestre11@gmail.com>
2022-05-31 14:57:25 -0300

    feat(middleware): create middleware for error handle and validation
    - implement login validation middleware
    - implement generic error handler middleware

---
+++ app/backend/src/middleware/validators/UserValidator.ts
@@ -0,0 +1,28 @@
+import type { Request, Response, NextFunction } from 'express';
+import { BadRequestError } from 'restify-errors';
+import * as joi from 'joi';
+
+class UserValidator {
+  private static loginValidationSchema = joi.object({
+    email: joi.string().email().required(),
+    password: joi.string().required(),
+  });
+
+  public static validateLogin(
+    req: Request,
+    _res: Response,
+    next: NextFunction,
+  ): void {
+    const { error } = UserValidator.loginValidationSchema.validate(req.body);
+
+    if (error) {
+      const err = new BadRequestError('All fields must be filled');
+
+      return next(err);
+    }
+
+    return next();
+  }
+}
+
+export default UserValidator;
