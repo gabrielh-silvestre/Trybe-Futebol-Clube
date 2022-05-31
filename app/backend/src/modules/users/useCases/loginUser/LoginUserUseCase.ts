df1d77d0ce764f73e7de62bbe74dfbe15d992ca8
Gabriel Silvestre <gabriel.h.silvestre11@gmail.com>
2022-05-30 23:09:26 -0300

    feat(modules/users/useCases/loginUser): implement auth and encrypt services
    - internalize features on LoginUserUseCase
    - delete utils

--- app/backend/src/modules/users/useCases/loginUser/LoginUserUseCase.ts
+++ app/backend/src/modules/users/useCases/loginUser/LoginUserUseCase.ts
@@ -1,9 +1,15 @@
 import { UnauthorizedError } from 'restify-errors';
+import { StatusCodes } from 'http-status-codes';
 
-import { IUsersRepository } from '../../../../@types/interfaces';
-import { User, UserLoginReturn, SuccessReturn } from '../../../../@types/types';
-
-import loginReturnConstructor from '../../../../utils/loginReturnConstructor';
+import type {
+  User,
+  UserLoginReturn,
+  SuccessReturn,
+  UserToken,
+} from '../../../../@types/types';
+import type { IUsersRepository } from '../../../../@types/interfaces';
+import type { IAuthService } from '../../../../services/Auth';
+import type { IEncryptService } from '../../../../services/Encrypt';
 
 type IRequest = {
   email: string;
@@ -11,7 +17,11 @@ type IRequest = {
 };
 
 class LoginUserUseCase {
-  constructor(private readonly userRepository: IUsersRepository) {}
+  constructor(
+    private readonly userRepository: IUsersRepository,
+    private readonly authService: IAuthService,
+    private readonly encryptService: IEncryptService,
+  ) {}
 
   private async userExists(email: string): Promise<User | never> {
     const user = await this.userRepository.findByEmail(email);
@@ -23,21 +33,54 @@ class LoginUserUseCase {
     return user;
   }
 
+  private async passwordIsCorrect(
+    password: string,
+    userPassword: string,
+  ): Promise<boolean | never> {
+    const isPasswordCorrect = await this.encryptService.verify(
+      password,
+      userPassword,
+    );
+
+    if (!isPasswordCorrect) {
+      throw new UnauthorizedError('Incorrect email or password');
+    }
+
+    return true;
+  }
+
+  private generateToken({ id, username, role }: UserToken): string {
+    return this.authService.generate({ id, username, role });
+  }
+
+  private static returnConstructor(
+    foundUser: User,
+    token: string,
+  ): UserLoginReturn {
+    return {
+      user: {
+        id: foundUser.id,
+        username: foundUser.username,
+        email: foundUser.email,
+        role: foundUser.role,
+      },
+      token,
+    };
+  }
+
   public async execute({
     email,
     password,
   }: IRequest): Promise<SuccessReturn<UserLoginReturn>> {
     const foundUser = await this.userExists(email);
 
-    // TODO: create a service to compare encrypted passwords
-    if (foundUser.password !== password) {
-      throw new UnauthorizedError('Incorrect email or password');
-    }
+    await this.passwordIsCorrect(password, foundUser.password);
 
-    // TODO: create a service to generate a token
+    const token = this.generateToken(foundUser);
+
     return {
-      statusCode: 200,
-      data: loginReturnConstructor(foundUser, 'token'),
+      statusCode: StatusCodes.OK,
+      data: LoginUserUseCase.returnConstructor(foundUser, token),
     };
   }
 }
