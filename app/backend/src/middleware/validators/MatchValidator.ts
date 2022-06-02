7f3caa516b439abd4a52a42c91448813085bdace
Gabriel Silvestre <gabriel.h.silvestre11@gmail.com>
2022-06-02 16:38:13 -0300

    feat(middleware/validators/MatchValidator): check if match exists

--- app/backend/src/middleware/validators/MatchValidator.ts
+++ app/backend/src/middleware/validators/MatchValidator.ts
@@ -1,6 +1,8 @@
 import type { Request, Response, NextFunction } from 'express';
-import { BadRequestError } from 'restify-errors';
+import { BadRequestError, NotFoundError } from 'restify-errors';
 import * as joi from 'joi';
+
+import MatchModel from '../../database/models/MatchModel';
 
 class MatchValidator {
   private static createValidationSchema = joi.object({
@@ -20,6 +22,23 @@ class MatchValidator {
 
     if (error) {
       const err = new BadRequestError(error.details[0].message);
+
+      return next(err);
+    }
+
+    return next();
+  }
+
+  public static async validateMatchExists(
+    req: Request,
+    _res: Response,
+    next: NextFunction,
+  ): Promise<void> {
+    const id = Number(req.params.id);
+    const taskExists = await MatchModel.findByPk(id);
+
+    if (!taskExists) {
+      const err = new NotFoundError('Match not found');
 
       return next(err);
     }
