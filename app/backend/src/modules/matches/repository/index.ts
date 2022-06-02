651d5721b26187d201e43063cc2942ebd8764a47
Gabriel Silvestre <gabriel.h.silvestre11@gmail.com>
2022-06-02 16:23:21 -0300

    feat(modules/matches/repository): implement update method
    - only update teams goals and in progress status

--- app/backend/src/modules/matches/repository/index.ts
+++ app/backend/src/modules/matches/repository/index.ts
@@ -1,9 +1,11 @@
 import { Op } from 'sequelize';
+
 import type { IMatchesRepository } from '../../../@types/interfaces/match.interfaces';
 import type {
   MatchAttributes,
   MatchCreation,
   MatchReturn,
+  MatchUpdate,
 } from '../../../@types/types';
 
 import MatchModel from '../../../database/models/MatchModel';
@@ -33,6 +35,28 @@ class MatchesRepository implements IMatchesRepository {
     const result = await this.model.create(matchCreation);
 
     return result.get();
+  }
+
+  async update(id: number, newScore: MatchUpdate): Promise<MatchAttributes> {
+    const match = (await this.model.findByPk(id)) as MatchModel;
+
+    const { awayTeamGoals, homeTeamGoals, inProgress } = match.get();
+
+    match.set({
+      awayTeamGoals: newScore.awayTeamGoals
+        ? newScore.awayTeamGoals
+        : awayTeamGoals,
+      homeTeamGoals: newScore.homeTeamGoals
+        ? newScore.homeTeamGoals
+        : homeTeamGoals,
+      inProgress: newScore.inProgress === undefined
+        ? inProgress
+        : newScore.inProgress,
+    });
+
+    await match.save();
+
+    return match.get();
   }
 }
 
