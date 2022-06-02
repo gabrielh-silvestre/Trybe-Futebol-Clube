646f6cf87113db4d542e4034fbe2c7315e4bffa8
Gabriel Silvestre <gabriel.h.silvestre11@gmail.com>
2022-06-02 00:16:24 -0300

    feat(routes/matchRouter): implement endpoint POST /matches

--- app/backend/src/routes/matchRouter.ts
+++ app/backend/src/routes/matchRouter.ts
@@ -1,11 +1,26 @@
 import { Router } from 'express';
 
 import getAllMatchesController from '../modules/matches/useCases/getAllMatches';
+import createNewMatchController from '../modules/matches/useCases/createNewMatch';
+
+import MatchValidator from '../middleware/validators/MatchValidator';
+import TeamValidator from '../middleware/validators/TeamValidator';
+import AuthValidator from '../middleware/auth';
 
 const router = Router();
 
 router.get('/', (req, res, next) => {
   getAllMatchesController.handle(req, res, next);
 });
+
+router.post(
+  '/',
+  AuthValidator.handle,
+  MatchValidator.validateCreate,
+  TeamValidator.validateTeamExists,
+  (req, res, next) => {
+    createNewMatchController.handle(req, res, next);
+  },
+);
 
 export default router;
