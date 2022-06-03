3f7480559a3fe9b3229ba95e765b1ce99c1852ae
Gabriel Silvestre <gabriel.h.silvestre11@gmail.com>
2022-06-03 19:09:01 -0300

    feat(@types/interfaces/leaderboard): create BoardBuilder interface
    - use BoardBuilder as a util

--- app/backend/src/@types/interfaces/leaderboard.interfaces.ts
+++ app/backend/src/@types/interfaces/leaderboard.interfaces.ts
@@ -1,7 +1,18 @@
-import { LeaderBoardAttributes, LeaderBoardOptions } from '../types';
+import type {
+  LeaderBoardAttributes,
+  LeaderBoardOptions,
+  TeamMatchesAttributes,
+} from '../types';
 
 interface LeaderBoardRepository {
   getAll(option?: LeaderBoardOptions): Promise<LeaderBoardAttributes[]>;
 }
 
+interface BoardBuilder {
+  home: (teamMatches: TeamMatchesAttributes[]) => LeaderBoardAttributes[];
+  away: (teamMatches: TeamMatchesAttributes[]) => LeaderBoardAttributes[];
+  all: (teamMatches: TeamMatchesAttributes[]) => LeaderBoardAttributes[];
+}
+
 export type ILeaderBoardRepository = LeaderBoardRepository;
+export type IBoardBuilder = BoardBuilder;
